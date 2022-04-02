5f7a2ef489e5ab73c34b0eeacc3a131af99e6202
vicore6 <vicore6@gmail.com>
2022-04-02 17:26:35 +0300

    v1

---
+++ src/test/java/ru/netology/MobileBankApiTestV1.java
@@ -0,0 +1,20 @@
+package ru.netology;
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
+
+class MobileBankApiTestV1 {
+    @Test
+    void shouldReturnDemoAccounts() {
+        given()
+                .baseUri("http://localhost:9999/api/v1")
+                .when()
+                .get("/demo/accounts")
+                .then()
+                .statusCode(201)
+                .body(matchesJsonSchemaInClasspath("accounts.schema.json"))
+        ;
+    }
+}
---
+++ src/test/java/ru/netology/MobileBankApiTestV5.java
@@ -0,0 +1,24 @@
+package ru.netology.rest;
+
+import io.restassured.http.ContentType;
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
+import static org.hamcrest.Matchers.*;
+
+class MobileBankApiTestV5 {
+    @Test
+    void shouldReturnDemoAccounts() {
+        given()
+                .baseUri("http://localhost:9999/api/v1")
+                .when()
+                .get("/demo/accounts")
+                .then()
+                .statusCode(200)
+                .contentType(ContentType.JSON)
+                .body("every{ it.balance >= 0 }", is(true))
+        ;
+    }
+}
